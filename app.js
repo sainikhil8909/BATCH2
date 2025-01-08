0d2dc05bc2fb6fb6722aecd718af4fc0e01dc143
BATCH2 <sainikhil8909@gmail.com>
2025-01-08 10:37:01 +0530

    pushing the codes to repo

---
+++ app.js
@@ -0,0 +1,23 @@
+let str1 = "double quotes";
+console.log(str1 , typeof(str1));
+
+ let str2 = "Single quote";
+ console.log(str2 , typeof(str2));
+
+ let str3 = "template literals";
+ console.log(str3 , typeof(str3));
+
+ let bool1 = true;
+ console.log(bool1 , typeof(bool1));
+
+let bool2 = false;
+console.log(bool2 , typeof(bool2));
+
+let num1 = 10;
+console.log(num1 , typeof(num1));
+
+let num2 = 10.2456;
+console.log(num2 , typeof(num2));
+
+let num3 = 10e3;
+console.log(num3 , typeof(num3));
---
+++ operators.js
@@ -0,0 +1,18 @@
+console.log(5+5);
+console.log("hi"+10+50);
+console.log("hi"+(10+50));
+console.log(10%2);
+let a=10;
+console.log(a++);
+console.log(--a);
+
+let b=10;
+console.log(b--);
+console.log(--b);
+
+//assignment operator
+let c = 4;
+console.log(c += 7);
+console.log(c -= 7);
+console.log(c *= 7);
+console.log(c /= 7);
