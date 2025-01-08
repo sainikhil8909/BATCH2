ba3ee4b9bdd4048307588fe0d9946fd4621377a5
BATCH2 <sainikhil8909@gmail.com>
2025-01-08 13:21:03 +0530

    new files added

---
+++ looping.js
@@ -0,0 +1,29 @@
+// for(let i=1; i<=5; i++){
+// //     console.log("JavaScript");
+// }
+
+// const start =1;
+// const end =100;
+// console.log("Even numbers");
+
+// write a javascript program to find the factorial of a number
+
+// let number = 1;
+//     for(let i=1; i<=5; i++){
+
+//     }
+
+
+// let number = 5; 
+// let factorial = 1;
+
+// for (let i = 1; i <= number; i++) {
+//     factorial *= i;
+// }
+
+// console.log("Factorial of " + number + " is " + factorial);
+
+// while loop
+let i=1;
+while(i>= 5);
+console.log("hello");
