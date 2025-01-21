e6c5915002be30e537d98d54758fe5fc3b333313
BATCH2 <sainikhil8909@gmail.com>
2025-01-21 20:40:05 +0530

    new files added

--- arrays.js
+++ arrays.js
@@ -11,8 +11,21 @@ let users = ["abc" , "def" , "ghi"];
 // });
 // console.log(x);
 
-let x = users.forEach((user,i)=>{
-    // console.log(user);
-    return user;
-});
-console.log(x);
+// let x = users.forEach((user,i)=>{
+//     // console.log(user);
+//     return user;
+// });
+
+let userData ={
+    name:"Sathvik",
+    age:25,
+    city:"Karimnagar"
+};
+console.log(userData.name , typeof(userdata));
+
+let x = JSON.stringify(userData);
+console.log(x , typeof(x));
+let y = JSON.parse(x);
+console.log(y);
+
+
